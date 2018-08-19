55b71d0a6f241042d510452d744b89762f84dda8
Yasu <yasu@pixiv.co.jp>
2018-08-19 21:22:07 +0900

    initial commit

---
+++ setup.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+set +eux
+git clone git://github.com/yyuu/pyenv.git ${HOME}/.pyenv
+cat << EOF >> ${HOME}/.profile
+export PYENV_ROOT=\${HOME}/.pyenv
+export PATH=\${PYENV_ROOT}/bin:\${PATH}
+eval "\$(pyenv init -)"
+EOF
+source ${HOME}/.profile
+sudo apt-get install -y \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline7 \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev
